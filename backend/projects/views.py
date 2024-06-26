1e0c0c879855b2994daf7d35265b10f49ff53b56
Kee Seow Han <seow.han@u.nus.edu>
2024-06-26 22:47:44 +0800

    Add git workflow

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 import requests
 
 def index(request):
-    return render(request, 'index.html')
+    response = requests.get('https://api.github.com/users/KSHan29/repos')
+    data = response.json()
+    return render(request, 'index.html', {'projects': data})
 
 def get_projects(request):
     response = requests.get('https://api.github.com/users/KSHan29/repos')
