4315401a77c3dda85794eca6d8188a7367432828
Kee Seow Han <seow.han@u.nus.edu>
2024-07-01 01:17:56 +0800

    Commit pending changes

--- backend/projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, get_projects
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('api/projects', get_projects, name='get_projects'),
-]
