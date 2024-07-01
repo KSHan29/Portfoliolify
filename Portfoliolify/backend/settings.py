98548b91db5a557207df8e929e502fe865a95dd3
Kee Seow Han <seow.han@u.nus.edu>
2024-07-01 23:49:44 +0800

    Add Project image

--- Portfoliolify/backend/settings.py
+++ Portfoliolify/backend/settings.py
@@ -125,6 +125,7 @@ LOGIN_REDIRECT_URL = 'fetch_github_profile'
 LOGOUT_REDIRECT_URL = 'home'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_GITHUB_REDIRECT_URI = 'http://localhost:8000/auth/complete/github/'
 
 
 
