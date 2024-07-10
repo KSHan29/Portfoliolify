764cfa9d68b2d4b3cb2f2d7c1e03551f02542257
Kee Seow Han <seow.han@u.nus.edu>
2024-07-10 19:59:28 +0800

    Add .env for secret key

--- Portfoliolify/backend/settings.py
+++ Portfoliolify/backend/settings.py
@@ -26,7 +26,6 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-^%(nelr!lta*-bdr!^=1g!j=bm@z$zcfrm!97ghc@3sfv40(l&'
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 OPENAI_API_KEY = config('OPENAI_API_KEY')
 
