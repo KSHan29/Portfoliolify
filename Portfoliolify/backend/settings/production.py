624276e5f639719108cc18db6de313a57d8cb5f5
KSHan29 <seow.han@outlook.com>
2024-07-31 21:23:47 +0800

    Deploy Django to Heroku

--- Portfoliolify/backend/settings/production.py
+++ Portfoliolify/backend/settings/production.py
@@ -1,29 +1,44 @@
 from .base import *
+import dj_database_url
+import django_heroku
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': config('SQL_DATABASE'),
-        # 'USER': config('SQL_USER'),
-        # 'PASSWORD': config('SQL_PASSWORD'),
-        # 'HOST': config('SQL_HOST'),
-        # 'PORT': config('SQL_PORT'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     # 'ENGINE': 'django.db.backends.postgresql',
+    #     # 'NAME': config('SQL_DATABASE'),
+    #     # 'USER': config('SQL_USER'),
+    #     # 'PASSWORD': config('SQL_PASSWORD'),
+    #     # 'HOST': config('SQL_HOST'),
+    #     # 'PORT': config('SQL_PORT'),
+    # }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        'LOCATION': 'redis://redis:6379/1',
+        "LOCATION": os.getenv('REDIS_URL', 'redis://localhost:6379/1'),
+        # 'LOCATION': 'redis://redis:6379/1',
         # "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+django_heroku.settings(locals())
--- Portfoliolify/entrypoint.sh
+++ Portfoliolify/entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 # Apply database migrations
+python manage.py flush --noinput
 python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
 
 # Create superuser if environment variables are set
-if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    python manage.py createsuperuser --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL || true
-fi
+# if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
+#     python manage.py createsuperuser --noinput \
+#         --username $DJANGO_SUPERUSER_USERNAME \
+#         --email $DJANGO_SUPERUSER_EMAIL || true
+# fi
 
 # Start the server using Gunicorn
 exec python manage.py runserver 0.0.0.0:8000
